48f0c2ece04dc4c526c0f721ace319af61bacd67
Rati Gvazbaia <rgvazbaia@gmail.com>
2022-12-08 12:46:11 +0400

    add smth

--- script/burger.js
+++ script/burger.js
@@ -2,6 +2,5 @@ let navigationBlock = document.getElementById("navigationWraper");
 let burgerBarBlock = document.getElementById("burger-Bar");
 
 burgerBarBlock.addEventListener("click", function () {
-    
   navigationBlock.classList.toggle("navActive");
 });
--- script/script.js
+++ script/script.js
@@ -1,41 +1,39 @@
-$(function(){
-   $('.multiple-items').slick({
-	infinite: true,
-	// რამდენი სურათი გამოჩნდეს
-	slidesToShow: 3,
-	// სლაიდის დროს რამდენი ელემნტით გადავიდეს 
-	slidesToScroll: 1,
-	// რომ გამოჩნდეს ღილაკები
-	arrows: true,
-	// რომ გამოჩნდეს ბურთულები
-	dots:true,
-	
-	// responsiv-ის კოდი
-	responsive: [
-	    {
-	      breakpoint: 769,
-	      settings: {
-			arrows: false,
-	        slidesToShow: 2,
-	        slidesToScroll: 1
-			
-			
-	      }
-	    },
-	    {
-	      breakpoint: 481,
-	      settings: {
-			arrows: false,
-	        slidesToShow: 1,
-	        slidesToScroll: 1
-	      }
-	    },
-		{
-			breakpoint: 1025,
-	      settings: {
-			arrows: false,
-		  }
-		}
-    ]
-  });    
+$(function () {
+  $(".multiple-items").slick({
+    infinite: true,
+    // რამდენი სურათი გამოჩნდეს
+    slidesToShow: 3,
+    // სლაიდის დროს რამდენი ელემნტით გადავიდეს
+    slidesToScroll: 1,
+    // რომ გამოჩნდეს ღილაკები
+    arrows: true,
+    // რომ გამოჩნდეს ბურთულები
+    dots: true,
+
+    // responsiv-ის კოდი
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 481,
+        settings: {
+          arrows: false,
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 1025,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
+  });
 });
