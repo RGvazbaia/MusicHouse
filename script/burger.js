98fde78043188f5f7728c79651804277548468fe
Rati Gvazbaia <rgvazbaia@gmail.com>
2022-12-08 12:44:49 +0400

    add burger

---
+++ script/burger.js
@@ -0,0 +1,7 @@
+let navigationBlock = document.getElementById("navigationWraper");
+let burgerBarBlock = document.getElementById("burger-Bar");
+
+burgerBarBlock.addEventListener("click", function () {
+    
+  navigationBlock.classList.toggle("navActive");
+});
