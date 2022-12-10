8120de13a80f7e26cb932bb083298683d3dd398e
Rati Gvazbaia <rgvazbaia@gmail.com>
2022-12-10 09:30:09 +0400

    add final version

--- script/alert.js
+++ script/alert.js
@@ -1,19 +1,16 @@
 function alertbutton() {
-    let x = document.querySelector("#borderdiv");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-
-  } 
-  
-
+  let x = document.querySelector("#borderdiv");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
 
 let xclose = document.querySelector(".xclass");
 
 xclose.addEventListener("click", btclose);
-function btclose(){
-let borderclass = document.querySelector("#borderdiv");
-borderclass.style.display = "none";
+function btclose() {
+  let borderclass = document.querySelector("#borderdiv");
+  borderclass.style.display = "none";
 }
